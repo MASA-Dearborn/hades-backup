d1669bea5faebed64198ff2f2333a2e13f7fa1b3
bobev64 <68569762+Bobev64@users.noreply.github.com>
2026-06-07 17:21:24 -0400

    BMP i2c functionality works

--- backup/src/main.cpp
+++ backup/src/main.cpp
@@ -9,15 +9,14 @@
 #include <LoRa.h>
 
 // Pin defines - TODO: get actual pins
-#define BMP_CS     0
-#define BMI_GYR_CS 0
-#define BMI_ACC_CS 0
-#define LIS_CS     0
+#define BMI_GYR_CS 22
+#define BMI_ACC_CS 22
+#define LIS_CS     19
 #define RFM_CS     0
 
-#define MOSI 0
-#define MISO 0
-#define CLK  0
+#define MOSI 11
+#define MISO 12
+#define CLK  13
 
 Adafruit_BMP5xx bmp;
 Adafruit_LIS2MDL lis = Adafruit_LIS2MDL(2);
@@ -25,46 +24,48 @@ Bmi088Accel accel(SPI, BMI_ACC_CS);
 Bmi088Gyro gyro(SPI,BMI_GYR_CS);
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
   while(!Serial) delay(1000);
+  Wire1.begin();
 
   // BMP setup, see https://github.com/adafruit/Adafruit_BMP5xx/blob/main/examples/bmp5xx_test/bmp5xx_test.ino
   // for more config options.
   
-  if (!bmp.begin(BMP_CS, &SPI)){
+  if (!bmp.begin(BMP5XX_ALTERNATIVE_ADDRESS, &Wire1)){
     Serial.println("NO BMP FOUND");
-    while(1) delay(1000);
+    while(1) delay(100);
   }
 
   // LIS setup
-  /*
+/*  
   lis.enableAutoRange(true);
   if(!lis.begin_SPI(LIS_CS)){
     Serial.println("NO LIS FOUND");
     while(1) delay(10);
   }
 
+
   int status;
   status = accel.begin();
-  */
+*/  
   
   // LoRa Setup
+  /*
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  */
 
 }
 
 void loop(){
-  if(bmp.dataReady()){
     bmp.performReading();
     Serial.print("Pressure: ");
     Serial.print(bmp.pressure);
     Serial.println(" hPa");
-  }
 
-  /*
+/*  
   sensors_event_t event;
   lis.getEvent(&event);
 
@@ -78,10 +79,9 @@ void loop(){
   Serial.print(accel.getAccelX_mss());
   Serial.print("\t");
   Serial.println(gyro.getGyroX_rads());
-  */
-
+*/
   // Send telemetry data to ground
-
+  /*
   // Declare array of data to send to ground
   int numOfThingsBeingSent;
   float data[numOfThingsBeingSent]; // Will have to fill array with what you're sending
@@ -90,6 +90,7 @@ void loop(){
   LoRa.beginPacket();
   LoRa.write((byte*)&data, sizeof(data));
   LoRa.endPacket();
+  */
 
   delay(1000);
 }
