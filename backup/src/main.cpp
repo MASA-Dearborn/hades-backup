36e2fe10373e87a0c059da2de5847157afabc954
bobev64 <68569762+Bobev64@users.noreply.github.com>
2026-06-07 20:16:05 -0400

    RFM Troubles

--- backup/src/main.cpp
+++ backup/src/main.cpp
@@ -12,7 +12,10 @@
 #define BMI_GYR_CS 22
 #define BMI_ACC_CS 22
 #define LIS_CS     19
-#define RFM_CS     0
+#define RFM_CS     23
+#define RFM_EN     2
+#define RFM_INT0   1
+#define RFM_RST    0
 
 #define MOSI 11
 #define MISO 12
@@ -30,12 +33,12 @@ void setup(){
 
   // BMP setup, see https://github.com/adafruit/Adafruit_BMP5xx/blob/main/examples/bmp5xx_test/bmp5xx_test.ino
   // for more config options.
-  
+/*  
   if (!bmp.begin(BMP5XX_ALTERNATIVE_ADDRESS, &Wire1)){
     Serial.println("NO BMP FOUND");
     while(1) delay(100);
   }
-
+*/
   // LIS setup
 /*  
   lis.enableAutoRange(true);
@@ -50,21 +53,27 @@ void setup(){
 */  
   
   // LoRa Setup
-  /*
-  if (!LoRa.begin(433E6)) {
+
+  pinMode(RFM_EN, OUTPUT);
+  
+
+  LoRa.setPins(RFM_CS, RFM_RST, RFM_INT0);
+  if (!LoRa.begin(433E6)){
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (100);
   }
-  */
 
 }
 
 void loop(){
+
+  Serial.println("Initialized successfully");
+/*
     bmp.performReading();
     Serial.print("Pressure: ");
     Serial.print(bmp.pressure);
     Serial.println(" hPa");
-
+*/
 /*  
   sensors_event_t event;
   lis.getEvent(&event);
